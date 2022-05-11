62ec81cbf97e0e3701d60e783ba2169af478fe85
LEE-JAE-HYUN179 <leejaehyun1223@gmail.com>
2022-05-11 17:23:56 +0900

    first commit

---
+++ MPM_tutorial/1.water.py
@@ -0,0 +1,51 @@
+import taichi as ti
+
+ti.init(arch=ti.gpu)
+
+########## simulation parameter ##############
+particle_num = 512  ##python global variable : not updated in taichi kernel
+particle_mass = 1
+particle_initial_volume = 1
+grid_res = 128
+scene_len = 1
+grid_dx = scene_len / grid_res
+dt = 1e-3
+
+# material property
+particle_initial_rho = particle_mass / particle_initial_volume
+
+# taichi data
+# particle data
+ti_particle_pos = ti.Vector.field(3, ti.f32, particle_num)
+ti_particle_vel = ti.Vector.field(3, ti.f32, particle_num)
+ti_particle_F = ti.Matrix.field(3, 3, ti.f32, particle_num)
+ti_particle_C = ti.Matrix.field(3, 3, ti.f32, particle_num)  # for APIC
+ti_particle_Jp = ti.field(ti.f32, particle_num)
+
+# grid data
+ti_grid_vel = ti.Vector.field(3, ti.f32, shape=(grid_res, grid_res, grid_res))
+ti_grid_mass = ti.field(ti.f32, shape=(grid_res, grid_res, grid_res))
+
+
+##########################################
+
+@ti.func
+def compute_force():
+    pass
+
+
+@ti.kernel
+def init_grid():
+    for i, j, k in ti_grid_mass:
+        pass
+
+    pass
+
+
+@ti.kernel
+def step():
+    pass
+
+
+if __name__ == '__main__':
+    print("hello")
