a44c37ee776aef3816483e5ced0b0199675de81f
LEE JAE HYUN <leejaehyun1223@gmail.com>
2022-05-13 05:19:38 +0900

    water

--- MPM_tutorial/1.water.py
+++ MPM_tutorial/1.water.py
@@ -9,10 +9,21 @@ particle_initial_volume = 1
 grid_res = 128
 scene_len = 1
 grid_dx = scene_len / grid_res
+grid_inv_dx= 1/grid_dx
 dt = 1e-3
 
+gravity = 9.8
+
 # material property
-particle_initial_rho = particle_mass / particle_initial_volume
+rho_0 = particle_mass / particle_initial_volume
+E = 1
+nu = 1
+mu_0 = 1
+lambda_0 = 1
+bulk_modulus = 1
+gamma = 7
+
+
 
 # taichi data
 # particle data
@@ -33,6 +44,10 @@ ti_grid_mass = ti.field(ti.f32, shape=(grid_res, grid_res, grid_res))
 def compute_force():
     pass
 
+
+@ti.kernel
+def init():
+    pass
 
 @ti.kernel
 def init_grid():
