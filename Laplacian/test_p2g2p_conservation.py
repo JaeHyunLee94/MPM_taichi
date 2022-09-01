7f5acdccee152b394b78cdfbbd07fbe8cf109fa7
JHLee Media <leejaehyun1223@gmail.com>
2022-09-01 19:40:09 +0900

    complete FLIP

--- Laplacian/test_p2g2p_conservation.py
+++ Laplacian/test_p2g2p_conservation.py
@@ -1,19 +1,73 @@
 import taichi as ti
+
 ti.init(arch=ti.cuda)
 
+res = 128
+dx = 1 / res
+inv_dx = 1 / dx
+particle_num = 1
 
-res =128
-dx = 1/res
-inv_dx = 1/dx
+ti_some_quantity = ti.field(ti.f32, particle_num)
+ti_particle_pos = ti.Vector.field(3, ti.f32, particle_num)
+ti_grid_val = ti.field(ti.f32, shape=(res, res, res))
 
-ti_quantity  = ti.field(ti.f32, shape=(), dtype=ti.f32)
-ti_particle_pos = ti.Vector.field(3, ti.f32, 1)
-ti_grid_ = ti.field(ti.f32, shape=(res, res, res))
+
+@ti.kernel
+def init():
+    for p in range(particle_num):
+        ti_some_quantity[p] = 10
+        ti_particle_pos[p] = [0.4, 0.4, 0.4]
+    for i, j, k in ti_grid_val:
+        ti_grid_val[i, j, k] = 0
 
 
 @ti.kernel
 def p2g2p():
-    pass
+    for i, j, k in ti_grid_val:
+        ti_grid_val[i, j, k] = 0
+
+    for p in range(particle_num):
+        print("before p2g: ", ti_some_quantity[p])
+        Xp = ti_particle_pos[p] * inv_dx
+        base = int(Xp - 0.5)
+        fx = Xp - base
+        w = [0.5 * (1.5 - fx) ** 2, 0.75 - (fx - 1) ** 2, 0.5 * (fx - 0.5) ** 2]  # quadratic kernel
+
+        # grid_sum2=0
+        for i, j, k in ti.static(ti.ndrange(3, 3, 3)):
+            offset = ti.Vector([i, j, k])
+            weight = w[i].x * w[j].y * w[k].z
+            ti_grid_val[base + offset] += weight * ti_some_quantity[p]
+            # grid_sum2+=weight * ti_some_quantity[p]
+        # print("after p2g: ", grid_sum2)
+
+    grid_sum = 0.0
+    for i, j, k in ti_grid_val:
+        grid_sum += ti_grid_val[i, j, k]
+
+    print("after p2g,before g2p: ", grid_sum)
+    for p in range(particle_num):
+        # gather particle velocity
+        Xp = ti_particle_pos[p] * inv_dx
+        base = int(Xp - 0.5)
+        fx = Xp - base
+        w = [0.5 * (1.5 - fx) ** 2, 0.75 - (fx - 1) ** 2, 0.5 * (fx - 0.5) ** 2]  # quadratic kernel
+
+        # gathering
+        new_p = ti.zero(ti_some_quantity[p])
+        for i, j, k in ti.static(ti.ndrange(3, 3, 3)):
+            offset = ti.Vector([i, j, k])
+            weight = w[i].x * w[j].y * w[k].z
+            new_p += weight * ti_grid_val[base + offset]
+        ti_some_quantity[p] = new_p
+
+    for p in range(particle_num):
+        print("after g2p: ", ti_some_quantity[p])
+
 
 if __name__ == '__main__':
-    pass
+
+    init()
+    substep = 5
+    for s in range(substep):
+        p2g2p()
--- MPM_tutorial/1.water_FLIP.py
+++ MPM_tutorial/1.water_FLIP.py
@@ -1,11 +1,11 @@
 import taichi as ti
 
 arch = ti.vulkan if ti._lib.core.with_vulkan() else ti.cuda
-ti.init(arch=ti.cuda)
+ti.init(arch=arch)
 
 ########## simulation parameter ##############
 grid_res = 64
-particle_num = 2 * (grid_res ** 3) // 4  # 512 * 16  ##python global variable : not updated in taichi kernel
+particle_num = 8 * (grid_res ** 3)  ##python global variable : not updated in taichi kernel
 particle_rho = 1
 
 scene_len = 1
@@ -14,35 +14,39 @@ grid_inv_dx = 1 / grid_dx
 
 particle_initial_volume = (grid_dx * 0.5) ** 3
 particle_mass = particle_rho * particle_initial_volume
-dt = 4e-4
-
-# material property
-bulk_modulus = 10  ## lame's second coefficient
-gamma = 7  ## compressibility
-E = 4
+dt = 1e-3
+alpha = 0.95
 
 gravity = 9.8
 bound = 3
+E = 4
+# material property
+bulk_modulus = 1  ## lame's second coefficient
+gamma = 7  ## compressibility
 
 # taichi data
 # particle data
 ti_particle_pos = ti.Vector.field(3, ti.f32, particle_num)
 ti_particle_vel = ti.Vector.field(3, ti.f32, particle_num)
-ti_particle_C = ti.Matrix.field(3, 3, ti.f32, particle_num)
+# ti_particle_F = ti.Matrix.field(3, 3, ti.f32, particle_num)  ## need?
 ti_particle_Jp = ti.field(ti.f32, particle_num)
-#ti_kinetic_energy = ti.field(ti.f32, 100000)
-
+ti_particle_vel_grad = ti.Matrix.field(3, 3, ti.f32, particle_num)
 # grid data
+ti_grid_force = ti.Vector.field(3, ti.f32, shape=(grid_res, grid_res, grid_res))
 ti_grid_vel = ti.Vector.field(3, ti.f32, shape=(grid_res, grid_res, grid_res))
+ti_grid_vel_old = ti.Vector.field(3, ti.f32, shape=(grid_res, grid_res, grid_res))
 ti_grid_mass = ti.field(ti.f32, shape=(grid_res, grid_res, grid_res))
+
+# x2= ti.field(ti.f32)
+# t
+# ti.root.dense(ti.i, 5).place(x2)
 
 ##########################################
 
 particle_color = (0, 0.5, 1)
-particle_radius = 0.01
+particle_radius = 0.005
 
-desired_frame_dt = 1 / 30
-ti_frame = ti.field(ti.i32, shape=())
+desired_frame_dt = 1 / 60
 
 window = ti.ui.Window('Window Title', (1280, 720))
 scene = ti.ui.Scene()
@@ -53,7 +57,6 @@ canvas.set_background_color((1, 1, 1))
 
 @ti.kernel
 def init():
-    ti_frame[None] = 0
     # particle initialize
     for p in range(particle_num):
         ti_particle_pos[p] = [
@@ -61,13 +64,14 @@ def init():
             (ti.random() - 0.5) * 0.5 + 0.3,
             (ti.random() - 0.5) * 0.5 + 0.5,
         ]
-        ti_particle_Jp[p] = 0.9
+        ti_particle_Jp[p] = 1
         ti_particle_vel[p] = [0, 0, 0]
-        ti_particle_C[p] = ti.Matrix.zero(ti.f32, 3, 3)
+        ti_particle_vel_grad[p] = ti.Matrix.zero(ti.f32, 3, 3)
     # grid initialize
     for i, j, k in ti_grid_mass:
         ti_grid_mass[i, j, k] = 0
         ti_grid_vel[i, j, k] = [0, 0, 0]
+        ti_grid_vel_old[i, j, k] = [0, 0, 0]
 
 
 @ti.kernel
@@ -78,6 +82,7 @@ def substep():
         ti_grid_mass[i, j, k] = 0
     for i, j, k in ti_grid_vel:
         ti_grid_vel[i, j, k] = [0, 0, 0]
+        ti_grid_vel_old[i, j, k] = [0, 0, 0]
 
     # p2g
     for p in ti_particle_pos:
@@ -85,37 +90,39 @@ def substep():
         base = int(Xp - 0.5)
         fx = Xp - base
         w = [0.5 * (1.5 - fx) ** 2, 0.75 - (fx - 1) ** 2, 0.5 * (fx - 0.5) ** 2]  # quadratic kernel
+        dw = [fx - 1.5, -2 * (fx - 1), fx - 0.5]
 
-        # pressure = bulk_modulus * ((1 / ti_particle_Jp[p]) ** gamma - 1)
-        #
-        # stress = -ti.Matrix.identity(ti.f32, 3) * pressure
-        # affine = stress                   + particle_mass * ti_particle_C[p]
-        # stress = -dt * 4 * E * particle_initial_volume * (ti_particle_Jp[p] - 1) / grid_dx ** 2
-        stress = dt * 4 * (bulk_modulus * ((1 / ti_particle_Jp[p]) ** gamma - 1) * ti_particle_Jp[
-            p] * particle_initial_volume) / grid_dx ** 2
-        affine = ti.Matrix([[stress, 0, 0], [0, stress, 0], [0, 0, stress]]) + particle_mass * ti_particle_C[p]
+        pressure = bulk_modulus * ((1 / ti_particle_Jp[p]) ** gamma - 1)
+        stress = -pressure * ti.Matrix.identity(ti.f32, 3)
+        # stress=  -dt*4 * E * particle_initial_volume * (ti_particle_Jp[p] - 1) * ti.Matrix.identity(ti.f32)
+        fp = -ti_particle_Jp[p] * particle_initial_volume * stress
 
         # loop unrolling
         # scattering
-
         for i, j, k in ti.static(ti.ndrange(3, 3, 3)):
             offset = ti.Vector([i, j, k])
             weight = w[i].x * w[j].y * w[k].z
-            dpos = (offset - fx) * grid_dx
-            ti_grid_vel[base + offset] += weight * (particle_mass * ti_particle_vel[p] + affine @ dpos)
-            ti_grid_mass[base + offset] += weight * particle_mass
+            d_weight = ti.Vector([
+                grid_inv_dx * dw[i].x * w[j].y * w[k].z,
+                grid_inv_dx * w[i].x * dw[j].y * w[k].z,
+                grid_inv_dx * w[i].x * w[j].y * dw[k].z,
+            ])
 
+            ti_grid_vel[base + offset] += weight * (particle_mass * ti_particle_vel[p]) + dt * fp @ d_weight
+            ti_grid_vel_old[base + offset] += weight * (particle_mass * ti_particle_vel[p])
+            ti_grid_mass[base + offset] += weight * particle_mass
 
     # grid update
 
     for i, j, k in ti_grid_mass:
+
         if ti_grid_mass[i, j, k] > 0:
             ti_grid_vel[i, j, k] /= ti_grid_mass[i, j, k]
+            ti_grid_vel_old[i, j, k] /= ti_grid_mass[i, j, k]
             ti_grid_vel[i, j, k].y -= dt * gravity
 
         # cond = (I < bound) & (ti_grid_vel[I] < 0) | (I > grid_res - bound) & (ti_grid_vel[I] > 0)
         # ti_grid_vel[I] = ti.select(cond, 0, ti_grid_vel[I])
-
         if i < bound and ti_grid_vel[i, j, k].x < 0:
             ti_grid_vel[i, j, k].x = 0
         if i > grid_res - bound and ti_grid_vel[i, j, k].x > 0:
@@ -136,47 +143,44 @@ def substep():
         base = int(Xp - 0.5)
         fx = Xp - base
         w = [0.5 * (1.5 - fx) ** 2, 0.75 - (fx - 1) ** 2, 0.5 * (fx - 0.5) ** 2]  # quadratic kernel
+        dw = [fx - 1.5, -2 * (fx - 1), fx - 0.5]
 
+        # ti_particle_vel[p] = [0, 0, 0]
+        ti_particle_vel_grad[p] = ti.Matrix.zero(ti.f32, 3, 3)
+
+        old_v = ti.zero(ti_particle_vel[p])
         new_v = ti.zero(ti_particle_vel[p])
-        new_C = ti.zero(ti_particle_C[p])
+        new_v_grad = ti.zero(ti_particle_vel_grad[p])
 
         # gathering
         for i, j, k in ti.static(ti.ndrange(3, 3, 3)):
             offset = ti.Vector([i, j, k])
-            dpos = (offset - fx) * grid_dx
             weight = w[i].x * w[j].y * w[k].z
-
+            d_weight = ti.Vector([
+                grid_inv_dx * dw[i].x * w[j].y * w[k].z,
+                grid_inv_dx * w[i].x * dw[j].y * w[k].z,
+                grid_inv_dx * w[i].x * w[j].y * dw[k].z,
+            ])
             new_v += weight * ti_grid_vel[base + offset]
-            new_C += 4 * weight * ti_grid_vel[base + offset].outer_product(dpos) / grid_dx ** 2
+            old_v += weight * ti_grid_vel_old[base + offset]
+            new_v_grad += ti_grid_vel[base + offset].outer_product(d_weight)
 
         # particle update
-        ti_particle_vel[p] = new_v
-        ti_particle_C[p] = new_C
-
+        ti_particle_vel[p] = new_v + alpha * (ti_particle_vel[p] - old_v)
+        ti_particle_vel_grad[p] = new_v_grad
         ti_particle_pos[p] += dt * ti_particle_vel[p]
-        ti_particle_Jp[p] *= 1 + dt * ti_particle_C[p].trace()
+        ti_particle_Jp[p] *= 1 + dt * ti_particle_vel_grad[p].trace()
 
 
-# @ti.kernel
-# def calc_energy():
-#     K = 0.0
-#     for p in ti_particle_pos:
-#         K += 0.5 * particle_mass * (
-#                     ti_particle_vel[p][0] ** 2 + ti_particle_vel[p][1] ** 2 + ti_particle_vel[p][2] ** 2)
-#
-#     ti_kinetic_energy[ti_frame[None]] = K
+
 
 def render_gui():
     global particle_radius
     global particle_color
 
-    global E
     window.GUI.begin("Render setting", 0.02, 0.02, 0.4, 0.15)
     particle_color = window.GUI.color_edit_3("particle color", particle_color)
     particle_radius = window.GUI.slider_float("particle radius", particle_radius, 0.001, 0.1)
-    E = window.GUI.slider_float("E", E, 4, 1000)
-    if window.GUI.button("restart"):
-        init()
     window.GUI.end()
 
     window.GUI.begin("Simulation setting", 0.02, 0.19, 0.3, 0.1)
@@ -185,7 +189,7 @@ def render_gui():
 
 
 def render():
-    camera.track_user_inputs(window, movement_speed=0.01, hold_key=ti.ui.RMB)
+    camera.track_user_inputs(window, movement_speed=0.03, hold_key=ti.ui.RMB)
     scene.set_camera(camera)
 
     scene.point_light((0.5, 2, 0.5), (1, 1, 1))
@@ -202,18 +206,13 @@ if __name__ == '__main__':
     camera.up(0, 1, 0)
     camera.fov(55)
     camera.projection_mode(ti.ui.ProjectionMode.Perspective)
-    print(particle_num)
-    while window.running:
-        for s in range(5):
-            substep()
 
-        #calc_energy()
-        ti_frame[None] += 1
+    while window.running:
+        for s in range(int(5)):
+            substep()
 
         render()
         render_gui()
         window.show()
 
-
-    #print(ti_kinetic_energy)
     print("hello")
